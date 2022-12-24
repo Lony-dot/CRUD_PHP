8fe143df30ba7c3fd12eb08c7f3247406217108b
Lony <loonydarck@hotmail.com>
2022-12-24 14:25:39 -0300

    Incluindo o deletar na página Index, para cumprir sua função de deleção ao clicar nele.

--- proc_apagar_usuario.php
+++ proc_apagar_usuario.php
@@ -2,6 +2,6 @@
 
 session_start();
 include_once("conexao.php");
-$id = 1;
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 $result_ususario = "DELETE FROM  usuarios WHERE  id ='$id' ";
 $resultado_ususario = mysqli_query($conn, $result_ususario);
